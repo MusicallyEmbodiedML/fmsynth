2415bda26fd5d344c9b6a9b471779ce2bca9c8bb
Andrea Martelloni <a.martelloni@sussex.ac.uk>
2024-10-10 10:15:19 +0100

    Multiplier for phase in Euclidean

--- api/EuclideanSeq.hpp
+++ api/EuclideanSeq.hpp
@@ -12,9 +12,12 @@ class EuclideanSeq {
         int k;
         int offset_n;
         int offset_d;
+        int ph_mult;
     };
+    static constexpr size_t n_params = sizeof(params) / sizeof(int);
     
     EuclideanSeq();
+    EuclideanSeq(const std::vector<int> &n_choices);
     bool Process(float phasor);
     void SetParams(params p);
     void MapNNParams(std::vector<float> nn_params);
@@ -24,12 +27,17 @@ class EuclideanSeq {
 
  protected:
 
+    const std::vector<int> n_choices_;
+
     static constexpr float pulse_width_ = 0.5;
     bool is_init_;
     params params_;
     float offset_;
     static constexpr size_t n_probes_ = 2;
     float probes_[n_probes_];
+
+    int MakeN_(float param);
+    int MakeMul_(float param);
 };
 
 
--- src/EuclideanSeq.cpp
+++ src/EuclideanSeq.cpp
@@ -7,10 +7,23 @@ extern "C" {
 
 
 EuclideanSeq::EuclideanSeq() :
+    n_choices_{ 2, 4, 8, 12, 16 },
     is_init_(0),
     params_{ 0 },
     probes_{ 0 }
 {
+}
+
+
+EuclideanSeq::EuclideanSeq(const std::vector<int> &n_choices) :
+    n_choices_(n_choices),
+    is_init_(0),
+    params_{ 0 },
+    probes_{ 0 }
+{
+    for (auto &c: n_choices) {
+        xassert(c > 1);
+    }
 }
 
 
@@ -20,6 +33,12 @@ bool EuclideanSeq::Process(float phasor)
 
     if (is_init_) {
 
+        // Multiply and wrap around
+        phasor *= params_.ph_mult;
+        while (phasor >= 1){
+            phasor -= 1;
+        }
+        // Apply offset
         phasor += offset_;
         while (phasor >= 1){
             phasor -= 1;
@@ -52,8 +71,8 @@ void EuclideanSeq::SetParams(params p)
         p.n = 1;
     }
     // k
-    if (p.k >= p.n) {
-        std::printf("Eucl- k is > p, rounding to %d.\n", p.n);
+    if (p.k > p.n) {
+        std::printf("Eucl- k is > n, rounding to %d.\n", p.n);
         p.k = p.n;
     } else if (p.k <= 0) {
         std::printf("Eucl- k is < 1, rounding to 1.\n");
@@ -75,6 +94,11 @@ void EuclideanSeq::SetParams(params p)
         std::printf("Eucl- offset_n is < 0, rounding to 0.\n");
         p.offset_n = 0;
     }
+    // ph_mult
+    if (p.ph_mult <= 0) {
+        std::printf("Eucl- ph_mult is < 1, rounding to 1.\n");
+        p.ph_mult = 1;
+    }
 
     params_ = p;
     offset_ = static_cast<float>(p.offset_n) /
@@ -84,36 +108,73 @@ void EuclideanSeq::SetParams(params p)
 }
 
 
+int EuclideanSeq::MakeN_(float param)
+{
+    float n = param * n_choices_.size();
+    n = std::floor(n);
+    while (n >= n_choices_.size()) {
+        n -= 1.;
+    }
+    while (n < 0) {
+        n += 1.;
+    }
+
+    return n_choices_[static_cast<size_t>(n)];
+}
+
+
+int EuclideanSeq::MakeMul_(float param) {
+    static std::vector<int> mul_lookup{ 1, 2, 4 };
+
+    float n = param * mul_lookup.size();
+    n = std::floor(n);
+    while (n >= mul_lookup.size()) {
+        n -= 1.;
+    }
+    while (n < 0) {
+        n += 1.;
+    }
+
+    return mul_lookup[static_cast<size_t>(n)];
+}
+
+
 void EuclideanSeq::MapNNParams(std::vector<float> nn_params)
 {
+    xassert(nn_params.size() == n_params);
+
     constexpr float k_n_min = 1;
-    constexpr float k_n_max = 32;
-    constexpr float k_n_range = k_n_max - k_n_min;
+    // constexpr float k_n_max = 32;
+    // constexpr float k_n_range = k_n_max - k_n_min;
+    constexpr float k_offset_range = 8;
+    constexpr float k_k_scale = 1.;
 
     // n
-    float n = nn_params[0] * k_n_range;
-    n += k_n_min;
-    n = std::round(n);
+    float n = MakeN_(nn_params[0]);
 
     // k
     float k_range = n - k_n_min;
-    float k = nn_params[1] * k_range;
+    float k = nn_params[1] * (k_range * k_k_scale);
     k += k_n_min;
     k = std::round(k);
 
     // offset
-    float offset_d = nn_params[3] * k_n_range;
-    offset_d += k_n_min;
-    offset_d = std::round(offset_d);
+    // float offset_d = nn_params[3] * k_offset_range;
+    // offset_d += k_n_min;
+    // offset_d = std::round(offset_d);
+    float offset_d = k_offset_range;
     float offset_n_range = offset_d - k_n_min;
     float offset_n = nn_params[2] * offset_n_range;
 
+    // ph_mult
+    int ph_mult = MakeMul_(nn_params[4]);
 
     const params set_p {
         static_cast<int>(n),
         static_cast<int>(k),
         static_cast<int>(offset_n),
         static_cast<int>(offset_d),
+        ph_mult,
     };
 
     SetParams(set_p);
